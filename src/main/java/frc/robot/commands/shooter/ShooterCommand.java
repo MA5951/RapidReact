e5f2b51e2dbe567d06054c7a5ce578501a79ae90
Konata <42537566+konata-chan404@users.noreply.github.com>
2022-02-17 18:38:58 +0200

    shooter command group (not working!!)

--- src/main/java/frc/robot/commands/shooter/ShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShooterCommand.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Shooter;
+package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.shooter.Shooter;
@@ -11,12 +11,22 @@ public class ShooterCommand extends CommandBase {
   /** Creates a new ShooterCommand. */
   private Shooter shooter;
   private double setpoint;
+  private boolean stop;
 
   public ShooterCommand(double setpoint) {
     // Use addRequirements() here to declare subsystem dependencies.
     shooter = Shooter.getinstance();
     addRequirements(shooter);
     this.setpoint = setpoint;
+    this.stop = true;
+  }
+
+  public ShooterCommand(double setpoint, boolean stop) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    shooter = Shooter.getinstance();
+    addRequirements(shooter);
+    this.setpoint = setpoint;
+    this.stop = stop;
   }
 
   // Called when the command is initially scheduled.
@@ -34,7 +44,9 @@ public class ShooterCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooter.setMotor(0);
+    if (stop) {
+      shooter.setMotor(0);
+    }
   }
 
   // Returns true when the command should end.

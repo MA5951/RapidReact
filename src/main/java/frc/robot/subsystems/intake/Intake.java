c283af54f3951ac6bc4c249f9b1a8f77af0df11d
Konata <42537566+konata-chan404@users.noreply.github.com>
2022-02-19 16:31:42 +0200

    remove unnecessary solenoids

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.PortMap;
 import com.ma5951.utils.RobotConstants;
-import com.ma5951.utils.motor.MotorController;
 import com.ma5951.utils.motor.Piston;
 import com.ma5951.utils.motor.MA_TalonSRX;
 import com.ma5951.utils.subsystem.MotorSubsystem;
@@ -16,41 +15,36 @@ import com.ma5951.utils.subsystem.PistonSubsystem;
 public class Intake extends SubsystemBase implements MotorSubsystem, PistonSubsystem {
   /** Creates a new Instake. */
   private MA_TalonSRX intakeMotor;
-  private Piston intakeLeftPiston;
-  private Piston intakeRightPiston;
+  private Piston intakePiston;
   private static Intake intake;
 
   public Intake() {
     intakeMotor = new MA_TalonSRX(PortMap.intakeMotor, false, RobotConstants.KMOTOR_COAST);
-    intakeLeftPiston = new Piston(PortMap.intakeLeftPistonForward, PortMap.intakeLeftPistonReverse);
-    intakeRightPiston = new Piston(PortMap.intakeRightPistonForward, PortMap.intakeRightPistonReverse);
+    intakePiston = new Piston(PortMap.intakePistonForward, PortMap.intakePistonReverse);
   }
 
-  public void open(){
-    intakeRightPiston.set(true);
-    intakeLeftPiston.set(true);
+  public void open() {
+    intakePiston.set(true);
   }
 
-  public void close(){
-    intakeRightPiston.set(false);
-    intakeLeftPiston.set(false);
+  public void close() {
+    intakePiston.set(false);
   }
 
-  public boolean isOpen(){
-    return intakeRightPiston.get();
+  public boolean isOpen() {
+    return intakePiston.get();
   }
 
-  public void setVoltage (double voltege){
+  public void setVoltage(double voltege) {
     intakeMotor.setVoltage(voltege);
   }
 
-  public double getVoltage(){
+  public double getVoltage() {
     return intake.getVoltage();
   }
 
-
   public static Intake getinstance() {
-    if (intake == null){
+    if (intake == null) {
       intake = new Intake();
     }
     return intake;

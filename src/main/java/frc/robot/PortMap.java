256a1d28a2118e00518c8df3d56280a7416b8b28
EilonK05 <eilonlon.klein@gmail.com>
2022-02-20 17:47:24 +0200

    Port map update to match real robot

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -2,30 +2,30 @@ package frc.robot;
 
 public class PortMap {
     // Chassis
-    public static final int chassisLeftFrontMotor = 1;
-    public static final int chassisRightFrontMotor = 2;
-    public static final int chassisLeftRearMotor = 3;
+    public static final int chassisLeftFrontMotor = 2;
+    public static final int chassisRightFrontMotor = 3;
+    public static final int chassisLeftRearMotor = 5;
     public static final int chassisRightRearMotor = 4;
 
     // Intake
-    public static final int intakeMotor = 5;
+    public static final int intakeMotor = 6;
     public static final int intakePistonForward = 0;
     public static final int intakePistonReverse = 1;
     // Conveyor
-    public static final int conveyorLowerMotor = 6;
+    public static final int conveyorLowerMotor = 8;
     public static final int conveyorUpperMotor = 7;
     public static final int conveyorLowerIR = 1;
     public static final int conveyorUpperIR = 2;
 
     // Shooter
-    public static final int shooterLeftMotor = 8;
-    public static final int shooterRightMotor = 9;
+    public static final int shooterLeftMotor = 9;
+    public static final int shooterRightMotor = 10;
     public static final int shooterPistonForward = 4;
     public static final int shooterPistonReverse = 5;
 
     // Climb
-    public static final int climbExtensionMotor = 10;
-    public static final int climbRotationLeftMotor = 11;
-    public static final int climbRotationRightMotor = 12;
-    public static final int climbPassiveMotor = 13;
+    public static final int climbExtensionMotor = 11;
+    public static final int climbRotationLeftMotor = 12;
+    public static final int climbRotationRightMotor = 13;
+    public static final int climbPassiveMotor = 14;
 }

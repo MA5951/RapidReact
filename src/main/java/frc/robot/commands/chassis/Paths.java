7fb2e1ba4836321e3bad5087b58bce9322da5dab
Asaf Harel <45461162+Asaf-Harel@users.noreply.github.com>
2022-03-08 13:00:03 +0200

    Increased velocity in the RedPath turning

--- src/main/java/frc/robot/commands/chassis/Paths.java
+++ src/main/java/frc/robot/commands/chassis/Paths.java
@@ -20,7 +20,7 @@ public class Paths {
         public static final Path goToTheSecondBallPart1 = new Path(
                         Arrays.asList(new Waypoint(0, 0), new Waypoint(0, 0.6),
                                         new Waypoint(-1.2, 0)),
-                        0.2, 1.5, 1.5, 0.75, 0.3, 0.01);
+                        0.2, 1.5, 2, 1, 0.3, 0.035);
         public static final Path goToTheSecondBallPart2 = new Path(
                         Arrays.asList(new Waypoint(0, 0), new Waypoint(-0.2, 1.8)), 0.2, 1.5, 2.8, 1.2, 0.3, 0.04);
 
